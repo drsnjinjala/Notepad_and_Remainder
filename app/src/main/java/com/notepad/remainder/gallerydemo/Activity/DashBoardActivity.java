ccb5291380f22c9c335a2a0b330e917aee602e84
darshan <darshanjinjala9@gmail.com>
2023-12-09 11:21:05 +0530

    1) init.

---
+++ app/src/main/java/com/notepad/remainder/gallerydemo/Activity/DashBoardActivity.java
@@ -0,0 +1,73 @@
+package com.notepad.remainder.gallerydemo.Activity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.gallerydemo.Adapter.MyAdapter;
+import com.example.gallerydemo.R;
+import com.google.android.material.tabs.TabLayout;
+
+public class DashBoardActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dash_board);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+
+        tabLayout.addTab(tabLayout.newTab().setText("Image"));
+        tabLayout.addTab(tabLayout.newTab().setText("Folder"));
+
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final MyAdapter adapter = new MyAdapter(this, getSupportFragmentManager(),
+                tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+    }
+
+    private long BackPressedTime;
+
+    @Override
+    public void onBackPressed() {
+        if (BackPressedTime + 2000 > System.currentTimeMillis()) {
+            try {
+                finishAffinity();
+                System.exit(0);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            Toast.makeText(getBaseContext(), "Press back again to exit", Toast.LENGTH_SHORT).show();
+        }
+        BackPressedTime = System.currentTimeMillis();
+    }
+}
+
